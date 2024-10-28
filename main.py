741f0be5bcdb3da2dc298185119cea4a3075c07e
Gurmukh-Singh-4253 <gurmukh050304@gmail.com>
2024-10-28 12:51:50 +0530

    got basic stuff working

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-import csv
-import re
-import sys
+import pandas as pd
+from jinja2 import Environment, FileSystemLoader
+import subprocess
 
 # Some fixed rules for the CSV:
-# Must have a Name column, Father's name column, a Date of birth column,
+# Must have a Name column, roll number column,
 # and the rest of the columns would be subjects and marks.
 path = input("Enter the path to the CSV file: ")
 
-with open(path, "r") as f:
-    marks = f.readlines()
+data = pd.read_csv(path)
+subjects = [x for x in data.columns if x != "name" and x != "roll_no"]
+ranks = []
 
-# Marking all the headers
-headers = marks[0].split(',')
+for i in subjects:
+    data[f'rank_{i}'] = data[i].rank(ascending=False)
 
-# we initialize the data dictionary to hold the necessary columns.
-data = {}
-for i in headers:
-    data[i] = ""
+data['average_score'] = data[subjects].mean(axis=1)
+data['rank'] = data['average_score'].rank(ascending=False)
+
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('template.tex')
+
+for index, row in data.iterrows():
+    latex_code = template.render(
+        name=row['name'],
+        roll_no=row['roll_no'],
+        subjects=subjects,
+        score=row[subjects],
+        subrank=row[[f"rank_{i}" for i in subjects]],
+
+        average_score=row['average_score'],
+        rank=row['rank']
+    )
+
+    with open(f"report_card_{row['roll_no']}.tex", 'w') as f:
+        f.write(latex_code)
+
+    # Compile LaTeX
+    subprocess.run(['pdflatex', f"report_card_{row['roll_no']}.tex"])
