8f89dc02e54a83af07131543fbae31887475e5e0
Gurmukh-Singh-4253 <gurmukh050304@gmail.com>
2024-11-10 10:58:41 +0530

    improved functionality and performance

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
 import pandas as pd
+# from matplotlib import plot
 from jinja2 import Environment, FileSystemLoader
 import subprocess
-import sys
+import os
 
 # Some fixed rules for the CSV:
 # Must have a Name column, roll number column,
 # and the rest of the columns would be subjects and marks.
 path = input("Enter the path to the CSV file: ")
+if path == '':
+    path = 'test.csv'
 
 data = pd.read_csv(path)
 subjects = [x for x in data.columns if x != "name" and x != "roll_no"]
-ranks = []
 
 for i in subjects:
     data[f'rank_{i}'] = data[i].rank(ascending=False)
 
 data['average_score'] = data[subjects].mean(axis=1)
-data['rank'] = data['average_score'].rank(ascending=False)
+data['rank'] = data['average_score'].rank(method="min", ascending=False)
+
 
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template('template.tex')
+
+os.system("rm out/*")
 
 for index, row in data.iterrows():
     latex_code = template.render(
         name=row['name'],
         roll_no=row['roll_no'],
+        length=len(subjects),
         subjects=subjects,
-        score=row[subjects],
+        scores=row[subjects],
         subrank=row[[f"rank_{i}" for i in subjects]],
 
         average_score=row['average_score'],
         rank=row['rank']
     )
 
-    with open(f"report_card_{row['roll_no']}.tex", 'w') as f:
+    with open(f"out/report_card_{row['roll_no']}.tex", 'w') as f:
         f.write(latex_code)
 
     # Compile LaTeX
-    subprocess.run(['pdflatex', f"report_card_{row['roll_no']}.tex"])
+    subprocess.run(['pdflatex', '-output-directory=out', f"out/report_card_{row['roll_no']}.tex"])
+
+os.system("rm out/*.aux")
+os.system("rm out/*.tex")
+os.system("rm out/*.log")
