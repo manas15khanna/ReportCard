46519d54213006ae58f5255918145f81e4db8288
Gurmukh-Singh-4253 <gurmukh050304@gmail.com>
2024-11-22 13:02:00 +0530

    added overall statistics as text and improved output

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from jinja2 import Environment, FileSystemLoader
 import subprocess
 import os
 
+
 # Some fixed rules for the CSV:
 # Must have a Name column, roll number column,
 # and the rest of the columns would be subjects and marks.
@@ -14,15 +15,17 @@ subjects = [x for x in data.columns if x != "name" and x != "roll_no"]
 
 for i in subjects:
     data[f'rank_{i}'] = data[i].rank(method='min', ascending=False)
+    data[f'percentile_{i}'] = (len(data) - data[f'rank_{i}'])/len(data) * 100
 
 data['average_score'] = data[subjects].mean(axis=1)
 data['rank'] = data['average_score'].rank(method="min", ascending=False)
+data['percentile'] = (len(data) - data['rank']) / len(data) * 100
 
 
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template('template.tex')
 
-function_dict = {"min": min}
+function_dict = {"min": min, "int": int}
 template.globals.update(function_dict)
 
 os.system("mkdir out || rm out/*")
@@ -30,9 +33,12 @@ os.system("mkdir charts || rm charts/*")
 
 # Generating Graphs...
 for i in subjects:
-    counts, bins, patches = plt.hist(data[i], bins=[x for x in range(0, max(data[i])+5, 5)], color='skyblue')
+    counts, bins, patches = plt.hist(data[i], 
+                                     bins=[x for x in range(0, int(max(data[i]))+5, 5)], 
+                                     color='skyblue'
+                                     )
 
-    for j in range(5):
+    for j in range(len(patches)):
         for k in range(j):
             patches[k].set_facecolor('skyblue')
         patches[j].set_facecolor('blue')
@@ -42,6 +48,17 @@ for i in subjects:
         plt.savefig(f"charts/{i}_{j}.png")
     plt.cla()
 
+counts, bins, patches = plt.hist(data['average_score'], bins=[x for x in range(0, max(data[i])+5, 5)], color='pink')
+for i in range(len(patches)):
+    for j in range(i):
+        patches[j].set_facecolor('pink')
+    patches[i].set_facecolor('red')
+    plt.xlabel("Marks")
+    plt.ylabel("Frequency")
+    plt.savefig(f"charts/average_score_{i}.png")
+plt.cla()
+
+
 for index, row in data.iterrows():
     latex_code = template.render(
         name=row['name'],
@@ -50,9 +67,11 @@ for index, row in data.iterrows():
         subjects=subjects,
         scores=row[subjects],
         subrank=row[[f"rank_{i}" for i in subjects]],
+        subpercentile=row[[f'percentile_{i}' for i in subjects]],
 
         average_score=row['average_score'],
-        rank=row['rank']
+        rank=row['rank'],
+        percentile=row['percentile']
     )
 
     with open(f"out/report_card_{row['roll_no']}.tex", 'w') as f:
@@ -64,3 +83,19 @@ for index, row in data.iterrows():
     subprocess.run(['rm', f'out/report_card_{row['roll_no']}.log'])
     subprocess.run(['rm', f'out/report_card_{row['roll_no']}.tex'])
 
+summary = []
+for i in subjects:
+    summary.append(f"Statistics for subject {i}")
+    summary.append(f"Mean: {data[i].mean()}")
+    summary.append(f"Median: {data[i].median()}")
+    summary.append(f"Standard deviation: {data[i].std()}")
+    summary.append("")
+
+summary.append("Overall Statistics")
+summary.append(f"Mean: {data['average_score'].mean()}")
+summary.append(f"Median: {data['average_score'].median()}")
+summary.append(f"Standard deviation: {data['average_score'].std()}")
+
+with open("out/Comprehensive_analysis.txt", 'w') as f:
+    f.writelines(summary)
+    f.close()
